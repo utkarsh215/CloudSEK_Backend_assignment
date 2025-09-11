1be9691c325a83de674ec3749bbc68bac1154c73
utkarsh215 <115106879+utkarsh215@users.noreply.github.com>
2025-09-11 12:56:53 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
+  
   const token = req.header('x-auth-token');
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
